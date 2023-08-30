50dc87f59082d84b50620f0311b8e0d73ff38090
igor.kuzevanov@sibirix.ru <kuzevanovii@inbox.ru>
2023-08-30 15:01:16 +0700

    added calculations

---
+++ lib/calculation/mergingCalculation.dart
@@ -0,0 +1,76 @@
+import "../results.dart" show ParsingComponents, ParsingResult;
+import "../types.dart" show Meridiem;
+import "../utils/dayjs.dart" show assignSimilarDate, implySimilarDate;
+
+ParsingResult mergeDateTimeResult(
+    ParsingResult dateResult, ParsingResult timeResult) {
+  final result = dateResult.clone();
+  final beginDate = dateResult.start;
+  final beginTime = timeResult.start;
+  result.start = mergeDateTimeComponent(beginDate, beginTime);
+  if (dateResult.end != null || timeResult.end != null) {
+    final endDate = dateResult.end == null ? dateResult.start : dateResult.end;
+    final endTime = timeResult.end == null ? timeResult.start : timeResult.end;
+    final endDateTime = mergeDateTimeComponent(endDate, endTime);
+    if (dateResult.end == null &&
+        endDateTime.date().getTime() < result.start.date().getTime()) {
+      // For example,  "Tuesday 9pm - 1am" the ending should actually be 1am on the next day.
+
+      // We need to add to ending by another day.
+      final nextDayJs = endDateTime.dayjs().add(1, "day");
+      if (endDateTime.isCertain("day")) {
+        assignSimilarDate(endDateTime, nextDayJs);
+      } else {
+        implySimilarDate(endDateTime, nextDayJs);
+      }
+    }
+    result.end = endDateTime;
+  }
+  return result;
+}
+
+ParsingComponents mergeDateTimeComponent(
+    ParsingComponents dateComponent, ParsingComponents timeComponent) {
+  final dateTimeComponent = dateComponent.clone();
+  if (timeComponent.isCertain("hour")) {
+    dateTimeComponent.assign("hour", timeComponent.get("hour"));
+    dateTimeComponent.assign("minute", timeComponent.get("minute"));
+    if (timeComponent.isCertain("second")) {
+      dateTimeComponent.assign("second", timeComponent.get("second"));
+      if (timeComponent.isCertain("millisecond")) {
+        dateTimeComponent.assign(
+            "millisecond", timeComponent.get("millisecond"));
+      } else {
+        dateTimeComponent.imply(
+            "millisecond", timeComponent.get("millisecond"));
+      }
+    } else {
+      dateTimeComponent.imply("second", timeComponent.get("second"));
+      dateTimeComponent.imply("millisecond", timeComponent.get("millisecond"));
+    }
+  } else {
+    dateTimeComponent.imply("hour", timeComponent.get("hour"));
+    dateTimeComponent.imply("minute", timeComponent.get("minute"));
+    dateTimeComponent.imply("second", timeComponent.get("second"));
+    dateTimeComponent.imply("millisecond", timeComponent.get("millisecond"));
+  }
+  if (timeComponent.isCertain("timezoneOffset")) {
+    dateTimeComponent.assign(
+        "timezoneOffset", timeComponent.get("timezoneOffset"));
+  }
+  if (timeComponent.isCertain("meridiem")) {
+    dateTimeComponent.assign("meridiem", timeComponent.get("meridiem"));
+  } else if (timeComponent.get("meridiem") != null &&
+      dateTimeComponent.get("meridiem") == null) {
+    dateTimeComponent.imply("meridiem", timeComponent.get("meridiem"));
+  }
+  if (dateTimeComponent.get("meridiem") == Meridiem.PM &&
+      dateTimeComponent.get("hour") < 12) {
+    if (timeComponent.isCertain("hour")) {
+      dateTimeComponent.assign("hour", dateTimeComponent.get("hour") + 12);
+    } else {
+      dateTimeComponent.imply("hour", dateTimeComponent.get("hour") + 12);
+    }
+  }
+  return dateTimeComponent;
+}
---
+++ lib/calculation/years.dart
@@ -0,0 +1,35 @@
+/**
+ * Find the most likely year, from a raw number. For example:
+ * 1997 => 1997
+ * 97 => 1997
+ * 12 => 2012
+ */
+num findMostLikelyADYear(num yearNumber) {
+  if (yearNumber < 100) {
+    if (yearNumber > 50) {
+      yearNumber = yearNumber + 1900;
+    } else {
+      yearNumber = yearNumber + 2000;
+    }
+  }
+  return yearNumber;
+}
+
+num findYearClosestToRef(Date refDate, num day, num month) {
+  //Find the most appropriated year
+  final refMoment = dayjs(refDate);
+  var dateMoment = refMoment;
+  dateMoment = dateMoment.month(month - 1);
+  dateMoment = dateMoment.date(day);
+  dateMoment = dateMoment.year(refMoment.year());
+  final nextYear = dateMoment.add(1, "y");
+  final lastYear = dateMoment.add(-1, "y");
+  if (nextYear.diff(refMoment).abs() <
+      dateMoment.diff(refMoment).abs()) {
+    dateMoment = nextYear;
+  } else if (lastYear.diff(refMoment).abs() <
+      dateMoment.diff(refMoment).abs()) {
+    dateMoment = lastYear;
+  }
+  return dateMoment.year();
+}
