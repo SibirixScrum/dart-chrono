b4794d09ff6dc4adc670c4f86c793933b3ad3342
igor.kuzevanov@sibirix.ru <kuzevanovii@inbox.ru>
2023-09-06 16:24:05 +0700

    en locale step 3: refiners

--- lib/locales/en/refiners/ENMergeRelativeDateRefiner.dart
+++ lib/locales/en/refiners/ENMergeRelativeDateRefiner.dart
@@ -1,19 +1,21 @@
+import "package:chrono/chrono.dart";
+import "package:chrono/locales/en/constants.dart";
+import "package:chrono/ported/RegExpMatchArray.dart";
+import "package:chrono/types.dart";
+import "package:chrono/utils/timeunits.dart";
+
 import "../../../common/abstractRefiners.dart" show MergingRefiner;
 import "../../../results.dart"
     show ParsingComponents, ParsingResult, ReferenceWithTimezone;
-import "../constants.dart" show parseTimeUnits;
-import "../../../utils/timeunits.dart" show reverseTimeUnits;
 
 bool hasImpliedEarlierReferenceDate(ParsingResult result) {
   return result.text
-          .match(new RegExp(r'\s+(before|from)$', caseSensitive: false)) !=
-      null;
+      .match(new RegExp(r'\s+(before|from)$', caseSensitive: false));
 }
 
 bool hasImpliedLaterReferenceDate(ParsingResult result) {
   return result.text
-          .match(new RegExp(r'\s+(after|since)$', caseSensitive: false)) !=
-      null;
+      .match(new RegExp(r'\s+(after|since)$', caseSensitive: false));
 }
 
 /**
@@ -26,8 +28,25 @@ class ENMergeRelativeDateRefiner extends MergingRefiner {
     return new RegExp(r'^\s*$', caseSensitive: false);
   }
 
+  @override
+  ParsingResult mergeResults(String textBetween, ParsingResult currentResult,
+      ParsingResult nextResult, ParsingContext context) {
+    var timeUnits = parseTimeUnits(currentResult.text);
+    if (hasImpliedEarlierReferenceDate(currentResult)) {
+      timeUnits = reverseTimeUnits(timeUnits);
+    }
+    final components = ParsingComponents.createRelativeFromReference(
+        new ReferenceWithTimezone(nextResult.start.date()), timeUnits);
+    return new ParsingResult(
+        nextResult.reference,
+        currentResult.index,
+        '''${currentResult.text}${textBetween}${nextResult.text}''',
+        components);
+  }
+
+  @override
   bool shouldMergeResults(String textBetween, ParsingResult currentResult,
-      ParsingResult nextResult) {
+      ParsingResult nextResult, ParsingContext context) {
     // Dates need to be next to each other to get merged
     if (!textBetween.match(this.patternBetween())) {
       return false;
@@ -40,23 +59,42 @@ class ENMergeRelativeDateRefiner extends MergingRefiner {
       return false;
     }
     // make sure that <date2> implies an absolute date
-    return !!nextResult.start.get("day") &&
-        !!nextResult.start.get("month") &&
-        !!nextResult.start.get("year");
+    return nextResult.start.get(Component.day) != null &&
+        nextResult.start.get(Component.month) != null &&
+        nextResult.start.get(Component.year) != null;
   }
 
-  ParsingResult mergeResults(String textBetween, ParsingResult currentResult,
-      ParsingResult nextResult) {
-    var timeUnits = parseTimeUnits(currentResult.text);
-    if (hasImpliedEarlierReferenceDate(currentResult)) {
-      timeUnits = reverseTimeUnits(timeUnits);
-    }
-    final components = ParsingComponents.createRelativeFromReference(
-        new ReferenceWithTimezone(nextResult.start.date()), timeUnits);
-    return new ParsingResult(
-        nextResult.reference,
-        currentResult.index,
-        '''${ currentResult . text}${ textBetween}${ nextResult . text}''',
-        components);
-  }
+// bool shouldMergeResults(String textBetween, ParsingResult currentResult,
+//     ParsingResult nextResult) {
+//   // Dates need to be next to each other to get merged
+//   if (!textBetween.match(this.patternBetween())) {
+//     return false;
+//   }
+//   // Check if any relative tokens were swallowed by the first date.
+//
+//   // E.g. [<relative_date1> from] [<date2>]
+//   if (!hasImpliedEarlierReferenceDate(currentResult) &&
+//       !hasImpliedLaterReferenceDate(currentResult)) {
+//     return false;
+//   }
+//   // make sure that <date2> implies an absolute date
+//   return !!nextResult.start.get("day") &&
+//       !!nextResult.start.get("month") &&
+//       !!nextResult.start.get("year");
+// }
+//
+// ParsingResult mergeResults(String textBetween, ParsingResult currentResult,
+//     ParsingResult nextResult) {
+//   var timeUnits = parseTimeUnits(currentResult.text);
+//   if (hasImpliedEarlierReferenceDate(currentResult)) {
+//     timeUnits = reverseTimeUnits(timeUnits);
+//   }
+//   final components = ParsingComponents.createRelativeFromReference(
+//       new ReferenceWithTimezone(nextResult.start.date()), timeUnits);
+//   return new ParsingResult(
+//       nextResult.reference,
+//       currentResult.index,
+//       '''${ currentResult . text}${ textBetween}${ nextResult . text}''',
+//       components);
+// }
 }
